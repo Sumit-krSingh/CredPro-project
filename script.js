51941dbd77694571746435d7b13019ef52dd79cc
Sumit Singh <kr.sumit817@gmail.com>
2023-10-31 12:17:32 +0530

    delete logic and copyText with svg file add

--- script.js
+++ script.js
@@ -1,25 +1,65 @@
-// logic for print data in table
-let tb = document.querySelector(".tab1")
-let data = localStorage.getItem("credentials")
+function copyCred(txt) {
+navigator.clipboard.writeText(txt).then(
+    ()=>{
+        alert("Text copied to clipboard: " + txt);
+    },
+    ()=>{
+        alert("Text copied to clipboard failed");
 
-if(data == null){
-    tb.innerHTML= "no data to be show"
-}
-else{
+    },
+);
+  
+    // Provide user feedback (e.g., an alert)
+  }
+  
+// logic for delete data
+const deleteCredentials = (website) => {
+    let data = localStorage.getItem("credentials")
     let arr = JSON.parse(data);
-    for (let index = 0; index < arr.length; index++) {
-        const element = arr[index];
-str =`<tr>
-    <td>${element.website}</td>
-    <td>${element.username}</td>
-    <td>${element.password}</td>
-    <td>${"Delete"}</td>
+    arrUpdate = arr.filter((e) => {
+        return e.website != website
+    })
+    localStorage.setItem("credentials", JSON.stringify(arrUpdate))
+    alert(`successfully delete ${website}'s password`)
+    showcredentials()
+
+}
+// logic for print data in table
+let showcredentials = () => {
+    let tb = document.querySelector(".tab1")
+    let data = localStorage.getItem("credentials")
+
+    if (data == null) {
+        tb.innerHTML = "no data to be show"
+    }
+    else {
+        tb.innerHTML = ` <tr>
+    <th>Website</th>
+    <th>UserName</th>
+    <th>Password</th>
+    <th>Delete</th>
+
+  </tr>`
+        let arr = JSON.parse(data);
+        let str = ""
+        for (let index = 0; index < arr.length; index++) {
+            const element = arr[index];
+            str += `<tr>
+    <td>${element.website} <img onclick ="copyCred(${element.website})" src ="copyText.svg" alt ="copy button" width ="15" height ="10"></td>
+    <td>${element.username}<img onclick ="copyCred(${element.username})" src ="copyText.svg" alt ="copy button" width ="15" height ="10"></td>
+    <td>${element.password}<img onclick ="copyCred(${element.password})" src ="copyText.svg" alt ="copy button" width ="15" height ="10"></td>
+    <td><button class ="btnsm" onclick="deleteCredentials('${element.website}')">Delete</button></td>
 
 </tr>`
+        }
+        tb.innerHTML = tb.innerHTML + str
     }
-    tb.innerHTML = tb.innerHTML + str
+    website.value = "";
+    username.value = "";
+    password.value = "";
 }
 console.log("Hello sumit");
+showcredentials()
 // logic for store data in local storage with submit click
 
 document.querySelector(".btn").addEventListener("click", (e) => {
@@ -41,4 +81,6 @@ document.querySelector(".btn").addEventListener("click", (e) => {
         alert("credentials saved");
         localStorage.setItem("credentials", JSON.stringify(json))
     }
+    showcredentials()
+
 })
